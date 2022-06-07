7d6f2c1fdb40ad61c4e4bce03d170354d6f93a51
Marc-Antoine Blais <blaisma@live.fr>
2022-06-07 11:22:55 -0400

    still need work on javascript timer not working

--- app/javascript/controllers/tetris_controller.js
+++ app/javascript/controllers/tetris_controller.js
@@ -6,10 +6,67 @@ export default class extends Controller {
 
   connect() {
     console.log("tetris game")
+    this.cols = 11
+    this.rows = 21
+    this.end = this.rows * this.cols
 
+    this.straigtPiece = [
+      {radius: 2, x: 6, y: 3},
+      {radius: 1, x: 6, y: 2},
+      {radius: 1, x: 6, y: 1},
+      {radius: 2, x: 6, y: 0}
+    ]
+
+    this.piece = this.straigtPiece
+    this.#drawPiece()
+    const timer = setInterval(this.#moveDown(), 1000)
   }
 
   move(e) {
-    console.log(e)
+    if (e === 'ArrowLeft' || e === 'a') {
+      this.moveLeft()
+    } else if (e === 'ArrowRight' || e === 'd') {
+      this.moveRigth()
+    } else if (e === 'ArrowDown' || e === 's') {
+      this.moveDown()
+    }
+  }
+
+  #drawPiece() {
+    this.piece.forEach((block) => {
+      this.gridTargets.forEach((space) => {
+        if (space.id === `${block.x},${block.y}`) {
+          space.classList.remove("empty")
+          space.classList.add("grey")
+        }
+      })
+    })
+  }
+
+  #moveLeft() {
+
+  }
+
+  #moveRigth() {
+
+  }
+
+  #moveDown() {
+    const canMove = this.piece.every((block) => {
+      block.y < this.rows && document.getElementById(`${block.x},${block.y + 1}`).classList !== "taken"
+    })
+
+    if (canMove) {
+      this.piece.forEach((block) => {
+        this.gridTargets.forEach((space) => {
+          if (space.id === `${block.x},${block.y}`) {
+            space.classList.add("empty")
+            space.classList.remove("grey")
+          }
+        })
+        block.y += 1
+      })
+      this.#drawPiece()
+    }
   }
 }
