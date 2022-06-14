7749201d6d417cb5890601cf52cc0741e140f153
Marc-Antoine Blais <blaisma@live.fr>
2022-06-14 16:13:56 -0400

    added highscores

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get '/', to: 'games#index'
-  resources :games, only: %i[index show]
+  resources :games, only: %i[index show] do
+    resources :score, only: :create
+  end
 end
